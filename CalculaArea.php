47ab285e2d2f3da90d26bbc21880c54dca0e897c
Gil Moura <50829938+gilmoura82@users.noreply.github.com>
2021-02-21 19:04:51 -0300

    Add files via upload

---
+++ CalculaArea.php
@@ -0,0 +1,23 @@
+<?php
+
+
+// Número da área
+$numarea1 = 1;
+$numarea2 = 2;
+$numarea3 = 3;
+$numarea4 = 4;
+
+// Quantidade de quadrados por área
+$area1 = 1;
+$area2 = 5;
+$area3 = 13;
+$area4 = 25;
+
+function calculaArea($area,$num) { // dois parâmetros informados para área e quantidade de quadrados
+    echo "A área $area possui $num quadrado(s)"; // $area informa o número da área e $num informa q quantidade de quadrados
+}
+
+
+calculaArea($numarea2,$area2);
+
+?>
